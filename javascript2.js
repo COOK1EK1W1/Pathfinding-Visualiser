c73a0b2ec69a358b0f1cce3bb9930d57f4168a26
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2020-10-12 15:21:39 +0100

    lol

--- javascript2.js
+++ javascript2.js
@@ -4,15 +4,42 @@ const zoom = 20;
 const height = 680;
 const width = 1580;
 const tool = document.getElementById("tool");
+
 var start_coords = [-1, -1];
 var end_coords = [-1, -1];
 
-class Node{
-	constructor(x, y){
+const neighbours = [[-1, 1], [ 0, 1], [ 1, 1], [ 1, 0], [ 1,-1], [0,-1], [-1,-1], [-1, 0]];
+var open_nodes;
+var closed_nodes;
+
+class Node{ 
+	//G cost = distance from starting node
+	//H cost = distance from end node
+	//F cost = Gcost + Hcost
+	constructor(x, y, parent=null){
 		this.x = x;
 		this.y = y;
 		this.type = "empty";
-    }
+		this.parent = parent;
+
+
+		if (this.parent == null){this.G_cost = 0;}else{
+			var horiz = Math.abs(this.parent.x - x);
+			var vert = Math.abs(this.parent.y - y);
+			if (horiz == vert){this.G_cost = this.parent.get_G_cost() + 14;}else{this.G_cost = this.parent + 10;}
+		}
+		horiz = Math.abs(end_coords[0] - x);
+		vert = Math.abs(end_coords[1] - y);
+		if (horiz == vert){this.H_cost = horiz * 14;}else
+		if (horiz < vert){
+			this.H_cost = horiz * 14 + (vert - horiz) * 10;
+		}else{
+			this.H_cost = vert * 14 + (horiz - vert) * 10;
+		}
+		this.F_cost = this.H_cost + this.G_cost;
+		console.log(this.F_cost);
+	}
+
     update_square(type){
 		var colour;
 		this.type = type;
@@ -26,7 +53,7 @@ class Node{
 		ctx.fillRect(this.x * zoom, this.y * zoom, zoom, zoom);
 		ctx.rect(this.x * zoom, this.y * zoom, zoom, zoom);
 		ctx.stroke();
-    }
+	}
 }
 
 
@@ -52,6 +79,19 @@ function setup(){
 
 setup();
 
+var x = 0;
+var y = 0;
+function test(){
+	setInterval(step, 1);}
+
+function step(){
+	//var lowest_f_cost = open_nodes[0].get_fcost();
+	nodes[y][x].update_square("wall");
+	x++;
+	if (x == 79){y++;x=0;}
+}
+
+
 var mouseup = true;
 function setmouseup(){mouseup = true;}
 function setmousedown(){mouseup = false;}
@@ -69,7 +109,8 @@ function clickcanvas(event){
 
 		if (tool.value == "start")	{tile_type = "start";
 			if (start_coords[0] != -1){nodes[start_coords[1]][start_coords[0]].update_square("empty");} //revert the old start to empty
-			start_coords = [gridx, gridy];} else //update start with new coords
+			start_coords = [gridx, gridy];
+			open_nodes = [nodes[gridy][gridx]]} else //update start with new coords
 
 		if (tool.value == "finnish"){tile_type = "end";
 			if (end_coords[0] != -1){nodes[end_coords[1]][end_coords[0]].update_square("empty");} //revert the old start to empty
