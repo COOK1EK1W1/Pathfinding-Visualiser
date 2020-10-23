bab2ee576af003e9a7bbe75641d94781d4a6a3a3
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2020-10-23 13:08:43 +0100

    added dijkstra
    fixed some stuff aswell

--- javascript2.js
+++ javascript2.js
@@ -1,6 +1,7 @@
 const c = document.getElementById("grid"); //get elements
 const tool = document.getElementById("tool");
 const parent_arrow_checkbox = document.getElementById("parent_arrow_checkbox");
+const algorithm = document.getElementById("algorithm");
 const ctx = c.getContext("2d");
 
 const zoom = 20; //setup constants
@@ -19,6 +20,8 @@ var closed_nodes = [];
 var nodes = [];
 
 var algo_loop; //setup varialbes for the loops
+
+var algo_started = false;
 
 class Node{ 
 	//G cost = distance from starting node
@@ -110,15 +113,50 @@ function redraw(){
 	}
 }
 
+function clear_path(){
+	for (var y = 0; y < height / zoom; y++){ //update all the nodes
+		for (var x = 0; x < width / zoom; x++){
+			if (nodes[y][x].type == "closed" || nodes[y][x].type == "open" || nodes[y][x].type == "path"){
+				nodes[y][x].update_square("empty");
+			}
+		}
+	}
+	nodes[start_coords[1]][start_coords[0]].update_square("start");
+	nodes[end_coords[1]][end_coords[0]].update_square("end");
+	open_nodes = [];
+	closed_nodes = [];
+	algo_started = false;
 
+}
 
 setup();
 
-
-/*##############################A* Algotrithm##################################*/
-
 function start_loop(){
-	algo_loop = setInterval(step, 0);}
+	algo_loop = setInterval(step, 0);
+}
+
+function pause(){
+	clearInterval(algo_loop);
+}
+
+
+function is_traversable(x1, y1, x2, y2){
+	for (var node = 0; node < closed_nodes.length;node++){ //check if the node is in closed
+		if (closed_nodes[node].x == x2 && closed_nodes[node].y == y2){return false;}
+	}
+	
+	if ((x2 < 0 || x2 > (width / zoom) - 1 || //check if out of bounds
+		  y2 < 0 || y2 > (height / zoom) - 1)){return false;}
+
+	if (nodes[y2][x1].type == "wall" && 
+		nodes[y1][x2].type == "wall" &&
+		Math.abs(x2 - x1) == 1 && Math.abs(y2 - y1) == 1){return false;} //check if closed corner
+
+	if((nodes[y2][x2].type == "wall")){return false;} //check if it is a wall
+	
+	return true; //if it is not not traversable the it is traversable
+}
+
 
 function step(){
 	if (start_coords[0] == -1 || end_coords[0] == -1){
@@ -126,6 +164,16 @@ function step(){
 		clearInterval(algo_loop);
 		return;
 	}
+	if (algorithm.value == "A_Star"){A_Star();}
+	if (algorithm.value == "dijkstra"){dijkstra();}
+}
+/*##################################A* Algotrithm######################################*/
+function A_Star(){
+	if (algo_started == false){
+		open_nodes.push(nodes[start_coords[1]][start_coords[0]]);
+		algo_started = true;
+	}
+
 	if (open_nodes.length == 0){ //check if there is no path
 		alert("no path");
 		clearInterval(algo_loop);
@@ -148,7 +196,6 @@ function step(){
 	Current.update_square("closed");
 
 	if (Current.x == end_coords[0] && Current.y == end_coords[1]){
-		//probably stopt he loop idk how to do that yet
 		
 		Current.tracepath();
 		open_nodes = [];
@@ -158,40 +205,25 @@ function step(){
 	}else{
 		for (var neighbour = 0;neighbour < neighbours.length;neighbour++){
 			var neighbourcoords = [Current.x + neighbours[neighbour][0], Current.y + neighbours[neighbour][1]];
-			var in_closed = false;
-			for (var node = 0; node < closed_nodes.length;node++){
-				if (closed_nodes[node].x == neighbourcoords[0] && closed_nodes[node].y == neighbourcoords[1]){in_closed = true;}
-			}
 
-			if (!(neighbourcoords[0] < 0 ||
-				neighbourcoords[0] > (width / zoom) - 1 ||
-				neighbourcoords[1] < 0 ||
-				neighbourcoords[1] > (height / zoom) - 1)){
-				var closed_corner = false;
-				if (nodes[Current.y + neighbours[neighbour][1]][Current.x].type == "wall" && neighbour % 2 == 0 &&
-					nodes[Current.y][Current.x + neighbours[neighbour][0]].type == "wall"){closed_corner = true;}	
-				if(!(
-				nodes[neighbourcoords[1]][neighbourcoords[0]].type == "wall" ||
-				closed_corner ||
-				in_closed)){
-					var testing_node = nodes[neighbourcoords[1]][neighbourcoords[0]];
-					if (testing_node.type != "open"){
-						testing_node.parent = Current;
-						testing_node.init();
-						open_nodes.push(testing_node);
-						testing_node.update_square("open");
-						
-						
-					}else{
-						for(var node = 0; node < open_nodes.length; node++){
-							var newGcost = Current.G_cost;
-							if (neighbour % 2 == 0){newGcost+=14;}else{newGcost+=10;}
-							if (open_nodes[node].x == neighbourcoords[0] && open_nodes[node].y == neighbourcoords[1] && open_nodes[node].G_cost > newGcost){
-								open_nodes[node].G_cost = newGcost;
-								open_nodes[node].F_cost = open_nodes[node].G_cost + open_nodes[node].H_cost;
-								open_nodes[node].parent = Current;
-								open_nodes[node].update_square("open");
-							}
+			if (is_traversable(Current.x, Current.y, neighbourcoords[0], neighbourcoords[1])){
+				var testing_node = nodes[neighbourcoords[1]][neighbourcoords[0]];
+				if (testing_node.type != "open"){
+					testing_node.parent = Current;
+					testing_node.init();
+					open_nodes.push(testing_node);
+					testing_node.update_square("open");
+					
+					
+				}else{
+					for(var node = 0; node < open_nodes.length; node++){
+						var newGcost = Current.G_cost;
+						if (neighbour % 2 == 0){newGcost+=14;}else{newGcost+=10;}
+						if (open_nodes[node].x == neighbourcoords[0] && open_nodes[node].y == neighbourcoords[1] && open_nodes[node].G_cost > newGcost){
+							open_nodes[node].G_cost = newGcost;
+							open_nodes[node].F_cost = open_nodes[node].G_cost + open_nodes[node].H_cost;
+							open_nodes[node].parent = Current;
+							open_nodes[node].update_square("open");
 						}
 					}
 				}
@@ -199,10 +231,65 @@ function step(){
 		}
 	}
 }
-	/*##############################A* Algotrithm##################################*/
+/*##################################A* Algotrithm######################################*/
+
+/*##############################Dijkstra's Algotrithm##################################*/
+function dijkstra(){
+	if (algo_started == false){
+		algo_started = true;
+		for (var y = 0; y < nodes.length; y++){
+			for(var x = 0; x < nodes[y].length; x++){
+				nodes[y][x].G_cost = Infinity;
+			}
+		}
+		nodes[start_coords[1]][start_coords[0]].G_cost = 0;
+		open_nodes.push(nodes[start_coords[1]][start_coords[0]]);
+	}
+	var lowest_node = open_nodes[0]
+	for (var node = 0; node < open_nodes.length; node++){
+		if (open_nodes[node].G_cost < lowest_node.G_cost){
+			lowest_node = open_nodes[node];
+		}
+	}
+	var Current = lowest_node;
+	closed_nodes.push(Current);
+	for( var i = 0; i < open_nodes.length; i++){ 
+		if ( open_nodes[i] === Current) { 
+			open_nodes.splice(i, 1); 
+		}
+	}
+	Current.update_square("closed");
+	if (Current.x == end_coords[0] && Current.y == end_coords[1]){
+		Current.tracepath();
+		open_nodes = [];
+		closed_nodes = [];
+		clearInterval(algo_loop);
+		alert("Path Found");
+	}
+
+	for (var neighbour = 0; neighbour < neighbours.length; neighbour++){
+		var neighbourcoords = [Current.x + neighbours[neighbour][0], Current.y + neighbours[neighbour][1]];
+		
+		if (is_traversable(Current.x, Current.y, neighbourcoords[0], neighbourcoords[1])){
+			var neighbour_node = nodes[neighbourcoords[1]][neighbourcoords[0]];
+			var newdist =  Current.G_cost;
+			if (neighbour % 2 == 0){newdist += 14}else{newdist += 10;}
+			if (newdist < neighbour_node.G_cost){
+				if (neighbour_node.G_cost == Infinity){
+					open_nodes.push(neighbour_node);
+					neighbour_node.update_square("open");
+				}
+				neighbour_node.G_cost = newdist;
+				neighbour_node.parent = Current;
+			}
+		}
+	}
+}
+/*##############################Dijkstra's Algotrithm##################################*/
+
 
 var mouseup = true;
-function setmousedown(event){mouseup = false;mouse_draw(event);}	//check if mouse is down
+function setmousedown(event){mouseup = false; mouse_draw(event);}	//check if mouse is down
 function setmouseup(){mouseup = true;}								//check if mouse is up
 function mouse_move(event){mouse_draw(event);}						//check if mouse has moved
 
@@ -225,7 +312,6 @@ function mouse_draw(event){
 		if (tool.value == "start")	{tile_type = "start";
 			if (start_coords[0] != -1){nodes[start_coords[1]][start_coords[0]].update_square("empty");} //revert the old start to empty
 			start_coords = [gridx, gridy];//update start with new coords
-			open_nodes = [nodes[gridy][gridx]]; 
 		} else
 
 		if (tool.value == "finnish"){tile_type = "end";
